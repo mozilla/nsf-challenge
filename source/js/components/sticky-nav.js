655d652351f3b28253c99a1ad2f805127520d7f3
ScottDowne <scott.downe@gmail.com>
2017-01-27 15:20:55 -0500

    Nav style fixes

--- source/js/components/sticky-nav.js
+++ source/js/components/sticky-nav.js
@@ -2,20 +2,40 @@ var React = require(`react`);
 
 import StickyContainer from './sticky-container.js';
 
+var MenuLink = React.createClass({
+  onClick: function() {
+    this.props.activate(this.props.item);
+  },
+  render: function() {
+    var className = "";
+    if (this.props.active === this.props.item) {
+      className += "active";
+    }
+    return (
+      <a className={className} onClick={this.onClick} href={this.props.href}>{this.props.children}</a>
+    );
+  }
+});
+
 var StickyNav = React.createClass({
   getInitialState: function() {
     return {
       active: window.location.hash.replace("#", "") || ""
     };
   },
+  activate: function(active) {
+    this.setState({
+      active: active || ""
+    });
+  },
   render: function() {
     var active = this.state.active;
     return (
       <StickyContainer className="sticky-container" stickyFrom={() => 600}>
         <div className="container">
-          <a className={active === "about" ? "active" : ""} href="#about">About</a>
-          <a className={active === "events" ? "active" : ""} href="#events">Events</a>
-          <a className={active === "details" ? "active" : ""} href="#details">Details</a>
+          <MenuLink activate={this.activate} active={active} item="about" href="#about">About</MenuLink>
+          <MenuLink activate={this.activate} active={active} item="events" href="#events">Events</MenuLink>
+          <MenuLink activate={this.activate} active={active} item="details" href="#details">Details</MenuLink>
         </div>
       </StickyContainer>
     );
